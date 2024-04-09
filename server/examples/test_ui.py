914e3a73dd75562a46c845be6dd7dc1f0ff58c4c
Your Name <fdyaolu@gmail.com>
2024-04-09 16:49:14 -0700

    revert

--- server/examples/test_ui.py
+++ server/examples/test_ui.py
@@ -1,6 +1,6 @@
 from text_generation_server.utils.punica_utils import BatchedKvCache, BatchLenInfo, KvPool, KvCache
 from text_generation_server.models.custom_modeling.punica_llama_lora import BatchedLlamaLoraWeight, LlamaLoraWeight
-from text_generation_server.models.punica_causal_lm import PunicaLM, PunicaBatch
+from text_generation_server.models.punica_causal_lm import PunicaLM, LlamaForCausalLM
 
 from text_generation_server.pb import generate_pb2_grpc, generate_pb2
 
@@ -34,20 +34,15 @@ class MultiLora:
         self.stop_signal = threading.Event()
         self.base_model = "meta-llama/Llama-2-7b-hf"
         # Load base model
-        self.model = PunicaLM(
-            model_id="meta-llama/Llama-2-7b-hf",
-            lora_ids={
-                'gsm8k':'abcdabcd987/gsm8k-llama2-7b-lora-16',
-                'sqlctx':'abcdabcd987/sqlctx-llama2-7b-lora-16',
-                'viggo':'abcdabcd987/viggo-llama2-7b-lora-16',
-                }
-            )
+        self.model = PunicaLM(model_id="meta-llama/Llama-2-7b-hf",
+                               lora_ids=['abcdabcd987/gsm8k-llama2-7b-lora-16',
+                                         'abcdabcd987/sqlctx-llama2-7b-lora-16',
+                                         'abcdabcd987/viggo-llama2-7b-lora-16'])
         self.tokenizer = self.model.tokenizer
         self.rid = 0
 
         # Create text generation requests
-        self.reqctx = []
-        self.req_log = {}
+        self.reqctx: dict[tuple[str, str], generate_pb2.Request] = {}
         for model_name in lora_specs:
             for lora_or_base in ["lora", "base"]:
                 self._create_request(model_name, lora_or_base)
@@ -61,30 +56,25 @@ class MultiLora:
             lora_id = "empty"
         else:
             raise ValueError(f"Unknown lora_or_base={lora_or_base}")
-        
         prompt = random.choice(prompts)
         request = generate_pb2.Request(
             inputs=prompt,
             lora_id=lora_id,
             id=self.rid,
-            truncate=256,
+            truncate=1024,
             prefill_logprobs=True,
             top_n_tokens=20,
             parameters=generate_pb2.NextTokenChooserParameters(
                 temperature=0.9,
-                top_k=10,
+                top_k=0,
                 top_p=0.9,
-                typical_p=0.9,
-                repetition_penalty=1.1,
-                ),
+                repetition_penalty=1.1,),
             stopping_parameters=generate_pb2.StoppingCriteriaParameters(
-                max_new_tokens=256,
+                max_new_tokens=1024,
                 stop_sequences=[],
                 ignore_eos_token=True))
         self.rid += 1
-        self.reqctx.append(request)
-        self.req_log[str(request.id)] = f'{model_name}-{lora_or_base}'
-
+        self.reqctx[(model_name, lora_or_base)] = {"request": request, "is_prefill": False}
 
     def _delete_request(
         self,
@@ -102,33 +92,20 @@ class MultiLora:
         self,
         append_box: Callable[[str, str], None],
     ):
-        batch = None
         time.sleep(0.1)
-        for req in self.reqctx:
-            append_box(self.req_log[str(req.id)], req.inputs)
+        for (model_name, lora_or_base), reqctx in self.reqctx.items():
+            append_box(f"{model_name}-{lora_or_base}", reqctx.inputs)
 
         while not self.stop_signal.is_set():
-            #Sort by id.
-            if self.reqctx:
-                reqs = sorted(
-                    self.reqctx,
-                    key=lambda req: req.id,
-                )
-                new_batch = generate_pb2.Batch(id=int(time.time()), requests=reqs, size=len(reqs))
-                new_batch = PunicaBatch.from_pb(new_batch, self.tokenizer, torch.float16, torch.device("cuda"))
-                if batch is not None:
-                    batch = PunicaBatch.concatenate([batch, new_batch])
-                else:
-                    batch = new_batch
-                self.reqctx = []
+            # Put prefill requests first, then sort by lora_id.
+            reqs = sorted(
+                self.reqctx.items(),
+                key=lambda kv: (not kv[1]["is_prefill"], kv[1].lora_id),
+            )
 
-            if batch:
-                generations, batch, timing = self.model.generate_token(batch)
-                for gen in generations:
-                    if gen.generated_text is not None:
-                        append_box(self.req_log[str(gen.request_id)], gen.generated_text.text)
-                        #print(gen.request_id, gen.generated_text.text)
-                        
+
+
+
 
 class TailLog(Label):
     def __init__(self, **kwargs):
@@ -213,4 +190,4 @@ if __name__ == '__main__':
     thread.start()
     tui.run()
     logic.stop()
-    thread.join()
+    thread.join()   
