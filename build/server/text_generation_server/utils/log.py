ff771e6736642071e2011f02b3534861d82ef617
PeterYaoNYU <yy4108@nyu.edu>
2024-04-22 08:57:54 +0800

    initial example server local interface

--- build/server/text_generation_server/utils/log.py
+++
@@ -1,6 +0,0 @@
-from functools import lru_cache
-
-
-@lru_cache(10)
-def log_once(log, msg: str):
-    log(msg)
--- build/server/third_party/flashinfer/python/flashinfer/__init__.py
+++
@@ -1,4 +0,0 @@
-import torch
-from .ops import single_decode_with_kv_cache
-
-__version__ = "0.0.1"
